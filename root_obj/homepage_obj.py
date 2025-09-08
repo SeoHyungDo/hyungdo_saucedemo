9fb70b04d248ee515bcaa9344f0a1bb69fb267a5
SeoHyungDo <tjg106@gmail.com>
2025-09-08 17:40:13 +0900

    0908 mid update

--- root_obj/homepage_obj.py
+++ root_obj/homepage_obj.py
@@ -1,31 +1,42 @@
+from http.client import responses
+
 from selenium.webdriver.common.by import By
+import requests
 
 
-class saucedemo_home :
+class saucedemo_home:
     def __init__(self, driver): #생성자, 객체를 생성할 때 driver객체를 인자로 넣어 줄 것이라 생각하고 그 driver 객체를 받아 self.driver에 할당
         self.driver = driver
+        self.input_id = (By.ID, 'user-name')
+        self.input_pw = (By.ID, 'password')
+        self.login_button = (By.ID, 'login-button')
 
     login_logo = (By.XPATH,'//*[@id="root"]/div/div[1]')
-    apply_store = (By.XPATH,'//*[@id="subscribeHeader"]/li[2]/a')
-    apply_store_select_box = (By.XPATH, '//*[@id="subscribeHeader"]/li[2]/a')
-    apply_store_select_box_data = (By.XPATH, '//*[@id="subscribeHeader"]/li[2]/p')
-    login_button = (By.XPATH, '//*[@id="wa-top-bar"]/div/menu[1]/li[4]/a')
-    join_button = (By.XPATH, '//*[@id="wa-top-bar"]/div/menu[1]/li[3]/a')
 
     def login_logo_obj(self):
         return self.driver.find_element(*saucedemo_home.login_logo)
 
-    def apply_store_obj(self):
-        return self.driver.find_element(*saucedemo_home.apply_store)
+    def input_id_obj(self): # login > ID 경로
+        return self.driver.find_element(*saucedemo_home.input_id)
 
-    def apply_store_select_box_obj(self):
-        return self.driver.find_element(*saucedemo_home.apply_store_select_box)
+    def input_id_standard_user_login_obj(self, username, password): # ID에 standard_user 입력 상태
+        # send_keys를 하기 전에 JavaScript를 실행해서 자동 완성 팝업을 방지
+        self.driver.execute_script("document.getElementById('user-name').setAttribute('autocomplete', 'off');")
+        self.driver.execute_script("document.getElementById('password').setAttribute('autocomplete', 'off');")
 
-    def apply_store_select_box_data_obj(self):
-        return self.driver.find_elements(*saucedemo_home.apply_store_select_box_data)
+        self.driver.find_element(*self.input_id).send_keys(username)
+        self.driver.find_element(*self.input_pw).send_keys(password)
 
-    def login_button_obj(self):
+        self.driver.find_element(*self.login_button).click()
+
+    def input_pw_obj(self): # login > PW 경로
+        return self.driver.find_element(*saucedemo_home.input_pw)
+
+    def input_pw_exist_obj(self): # pw에 공통 비밀번호 입력 상태
+        return self.driver.find_element(*saucedemo_home.input_pw).send_keys("secret_sauce")
+
+    def login_button_obj(self): # 로그인 버튼 위치
         return self.driver.find_element(*saucedemo_home.login_button)
-
-    def join_button_obj(self):
-        return self.driver.find_element(*saucedemo_home.join_button)
+    
+    def login_button_click_obj(self): # pw에 공통 비밀번호 입력 상태
+        return self.driver.find_element(*saucedemo_home.login_button).click()
--- tests/test_loginpage.py
+++ tests/test_loginpage.py
@@ -4,14 +4,38 @@ from selenium.webdriver.common.by import By
 from utility.passclass import passclass
 from root_obj.homepage_obj import saucedemo_home
 from utility.BaseClass import BaseClass
+import requests
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 class Test_demotest(passclass) :
 
+    # URL로 정상 연결 되는지 확인한다.
+    def test_login_url_check(self):
+        url = "https://www.saucedemo.com/"
+        try:
+           responses = requests.get(url, timeout=10)
+           assert responses.status_code == 200
+
+        except requests.exceptions.RequestException as e:
+           pytest.fail(f"url 접속 실패 : {e}")
+
+    # 상단 로고 텍스트가 정상 노출되는지 확인한다.
     def test_login_logo(self) :
         saucedemo = saucedemo_home(self.driver)
         login_logo_text = saucedemo.login_logo_obj().text
         assert login_logo_text == "Swag Labs"
 
+    # 로그인 후 URL에 inventory.html이 있는지 확인한다.
+    def test_input_id(self) :
+        saucedemo = saucedemo_home(self.driver)
+        saucedemo.input_id_standard_user_login_obj("standard_user","secret_sauce") #standard User Login
+
+        wait = WebDriverWait(self.driver, 10)
+        inventory_container = wait.until(
+            EC.presence_of_element_located((By.ID, "inventory_container"))
+        )
+        assert "inventory.html" in self.driver.current_url
 
 #    def test_apply_store_selectbox_action(self):
 #        Coupang_main = coupang_main(self.driver)
